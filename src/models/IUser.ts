0b8dc7138f3572f911284a67f6db929dd6bcb55f
linokubemba <emmanuel.lino-kubemba@outlook.fr>
2025-03-12 14:07:25 +0100

    feat: add timestamps option in user schema

--- src/models/IUser.ts
+++ src/models/IUser.ts
@@ -11,14 +11,17 @@ export interface IUser extends Document {
   role: mongoose.Types.ObjectId | IRole
 }
 
-const UserSchema: Schema = new Schema({
-  _id: { type: mongoose.Schema.Types.ObjectId, index: true, required: true, auto: true },
-  email: { type: String, required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  pseudo: { type: String, required: true },
-  password: { type: String, required: true },
-  role: { type: mongoose.Types.ObjectId, required: true, ref: "Role" }
-});
+const UserSchema: Schema = new Schema(
+  {
+    _id: { type: mongoose.Schema.Types.ObjectId, index: true, required: true, auto: true },
+    email: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    pseudo: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: mongoose.Types.ObjectId, required: true, ref: "Role" }
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model<IUser>("User", UserSchema);
